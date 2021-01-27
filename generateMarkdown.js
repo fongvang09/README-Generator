efd62ca7f18627bed42585f5ba573152b6feefef
fongvang09 <koichi_vang@hotmail.com>
2021-01-26 20:03:20 -0600

    initial commit

---
+++ generateMarkdown.js
@@ -0,0 +1,23 @@
+// TODO: Create a function that returns a license badge based on which license is passed in
+// If there is no license, return an empty string
+function renderLicenseBadge(license) {
+  https://img.shields.io/github/license/fongvang09/README-Generator,
+  // https://licensebuttons.net/l/zero/1.0/80x15.png
+}
+
+// TODO: Create a function that returns the license link
+// If there is no license, return an empty string
+function renderLicenseLink(license) {https://img.shields.io/badge/License-Apache%202.0-blue.svg}
+
+// TODO: Create a function that returns the license section of README
+// If there is no license, return an empty string
+function renderLicenseSection(license) {}
+
+// TODO: Create a function to generate markdown for README
+function generateMarkdown(data) {
+  return `# ${data.title}
+
+`;
+}
+
+module.exports = generateMarkdown;
